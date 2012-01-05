5f436880f538cd5142747f03dcc7e37d5e15a55c
fern <PlayingWithPowershell@gmail.com>
2012-01-05 17:30:14 -0500

    First Commit
    Signed-off-by: fern <PlayingWithPowershell@gmail.com>

---
+++ GetProcessWebReport.ps1
@@ -0,0 +1,1 @@
+﻿Get-Process | Where {$_.ProcessName -imatch "chrome"} | Select-Object Name, CPU | Sort-Object CPU -Descending  | ConvertTo-Html -Head "<style> th {background-color: red;} td {border: 1px black solid; padding: 3px;} </style>" -Title "Processnames" | Out-File -FilePath C:\users\fern\desktop\test.html
