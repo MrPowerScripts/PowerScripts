36d5be83a7bb621a23dd6897f99d6000d188a6e3
fern <PlayingWithPowershell@gmail.com>
2012-01-07 14:46:03 -0500

    Moved some commands in EnvironmentVariables.ps1
    Signed-off-by: fern <PlayingWithPowershell@gmail.com>

--- Variables/EnvironmentVariables.ps1
+++ Variables/EnvironmentVariables.ps1
@@ -1,13 +1,17 @@
 ﻿#Environment variables can be called upon from the reserved variable $env:
+$env:NUMBER_OF_PROCESSORS
 $env:HOMEPATH
-$env:CLASSPATH
-$env:Path
+$env:COMPUTERNAME
+$env:OS
+
+
+#Variables in Powershell are evaluated first when inside " "
+Write-Host "$env:HOMEPATH\desktop"
 
 #Displays all environment variables
 Get-ChildItem env:
 
-#Variables in Powershell are evaluated first when inside " "
-Write-Host "$env:HOMEPATH\desktop"
+
 
 
 
