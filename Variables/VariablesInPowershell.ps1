796e9e69a5776027d71f8edaa6da4aeb723c7e44
fern <PlayingWithPowershell@gmail.com>
2012-01-07 13:00:46 -0500

    Added Variables/VariablesInPowershell.ps1
    Signed-off-by: fern <PlayingWithPowershell@gmail.com>

---
+++ Variables/VariablesInPowershell.ps1
@@ -0,0 +1,34 @@
+﻿
+#Variables start with a $ in Powershell
+
+#These are valid variables
+$Variable
+$ThisIsAVariableToo
+$ThisIsAlsoAVariable
+
+#You can also set variables like this
+Set-Variable -Name Variablename -Value VariableValue
+
+#Or like this - you don't need to delate types in powershell
+$Variable = 3
+$ThisIsAVariableToo = "it's true"
+$ThisIsAlsoAVariable = $True
+
+#Or even like this
+[String]$Season = "Winter"
+[int]$Month = "1"
+[bool]$TrueFalse = $False
+[array]$Things = ("1thing","2thing","3thing")
+
+cls #Clears the host
+
+#Write the values of our assigned variables to the host
+Write-Host $Variable
+Write-Host $ThisIsAVariableToo
+Write-Host $ThisIsAlsoAVariable
+Write-Host $Season
+Write-Host $Month
+Write-Host $TrueFalse
+Write-Host $Things
+
+
