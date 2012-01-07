f4b10061ae6078b8351ab1cde0a468c2db857834
fern <PlayingWithPowershell@gmail.com>
2012-01-07 11:09:08 -0500

    Added file structure, Pipelining/PipeliningGetProcessColoredOutput.ps1 added

---
+++ Pipelining/PipeliningGetProcessColoredOutput.ps1
@@ -0,0 +1,11 @@
+﻿cls
+
+Get-Process | Foreach {
+                                    
+                        if ($_.name -ilike "*chrome*") { Write-Host "$_" -ForegroundColor green} 
+                        Elseif ($_.name -ilike "*powershell*") { Write-Host "$_" -ForegroundColor red}
+                        else {Write-Host "$_"}
+
+                                    
+                      }
+
