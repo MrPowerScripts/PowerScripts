41f016fb6dede1cfe45b52b24bee4376349d1a6c
fern <PlayingWithPowershell@gmail.com>
2012-01-07 14:24:08 -0500

    Added EnvironmentVariables.ps1
    Signed-off-by: fern <PlayingWithPowershell@gmail.com>

---
+++ Variables/EnvironmentVariables.ps1
@@ -0,0 +1,13 @@
+﻿#Environment variables can be called upon from the reserved variable $env:
+$env:HOMEPATH
+$env:CLASSPATH
+$env:Path
+
+#Displays all environment variables
+Get-ChildItem env:
+
+#Variables in Powershell are evaluated first when inside " "
+Write-Host "$env:HOMEPATH\desktop"
+
+
+
