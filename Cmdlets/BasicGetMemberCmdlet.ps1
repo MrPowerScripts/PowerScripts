83eab79fd3e17fc34cfa94c7547851d44c9652ab
fern <PlayingWithPowershell@gmail.com>
2012-01-08 13:00:59 -0500

    Added BasicGetMemberCmdlet.ps1
    Signed-off-by: fern <PlayingWithPowershell@gmail.com>

---
+++ Cmdlets/BasicGetMemberCmdlet.ps1
@@ -0,0 +1,15 @@
+﻿
+
+#Using get member to find out information about the get-process cmdlet
+Get-Process | Get-Member
+
+#after finding out the Get-Process Cmdlet has a Company property we can use it with other Cmdlets to refine results
+Get-Process | Sort-Object Company | Where {$_.Company -ne $Null}
+
+$Variable = "String"
+
+#finding more information about our variable with Get-Member
+$Variable | Get-Member
+
+#Using the ToLower() method to change the characters in our string to lower case
+$Variable.ToLower()
