f102ac107738e75d02222d9a88c8afa4b81c7875
fern <PlayingWithPowershell@gmail.com>
2012-01-25 11:43:47 -0500

    added GetHost.ps1
    Signed-off-by: fern <PlayingWithPowershell@gmail.com>

---
+++ Cmdlets/GetHost.ps1
@@ -0,0 +1,8 @@
+﻿Get-Host
+
+
+$Ver = Get-Host | Select-Object Version
+cls
+If ($Ver -imatch "3.0") { Write-Host "This is powershell 3.0!"}
+Elseif  ($Ver -imatch "2.0") {  Write-Host "This is powershell 2.0!" }
+Elseif  ($Ver -imatch "1.0") {  Write-Host "This is powershell 1.0!" }
