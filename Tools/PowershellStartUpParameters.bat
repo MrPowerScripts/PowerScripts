bda9095624fcf426749406144ef1c14f67596786
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-04-03 16:28:30 -0400

    added PowershellStartUpParameters.bat
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Tools/PowershellStartUpParameters.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+Rem Run Powershell
+rem start /b powershell.exe -noexit -command "get-process"
+
+rem start /b powershell.exe -executionpolicy restricted
+
+rem start /b powershell.exe -file "C:\somefolder\somefile.ps1"
+
+rem start /b powershell.exe -NoLogo
+
+rem start /b powershell.exe -NoProfile
+
+rem start /b powershell.exe -sta
+
+rem start /b powershell.exe -version 2.0
+
+rem start /b powershell.exe -WindowStyle maximized 
+rem Normal, Minimized, Hidden
+
