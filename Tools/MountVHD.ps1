4143d57d419ef9602708aab35c23544ccaf5c11e
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-04-10 16:04:51 -0400

    added MountVHD.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Tools/MountVHD.ps1
@@ -0,0 +1,31 @@
+﻿
+
+function VHD-Mount {
+    Param (
+    [Parameter(Mandatory=$True)]
+    [string]$Path
+    )
+
+if (Test-Path $Path) {
+
+    $script = "SELECT VDISK FILE=`"$path`"`r`nATTACH VDISK"
+    $script | diskpart
+
+} Else {Throw "VHD Could not be found"}
+
+}
+
+function VHD-UnMount {
+    Param (
+    [Parameter(Mandatory=$True)]
+    [string]$Path
+    )
+
+if (Test-Path $Path) {
+
+    $script = "SELECT VDISK FILE=`"$path`"`r`nDETACH VDISK"
+    $script | diskpart
+
+} Else {Throw "VHD Could not be found"}
+
+}
