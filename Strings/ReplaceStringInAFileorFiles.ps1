0ac842a219ede38a5b29e5cb5db9bc8c3091895a
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-04-18 11:53:08 -0400

    added ReplaceStringInAFileorFiles.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Strings/ReplaceStringInAFileorFiles.ps1
@@ -0,0 +1,16 @@
+﻿#Change the string in one file
+$FileName = "C:\somefolder\somefile.txt"
+
+Get-Content "$FileName" | ForEach-Object { $_ -replace "@","O" } | Set-Content "C:\users\fern\Desktop\CleanGarden.txt" -Force
+
+
+#Change the string of multiple files
+Get-ChildItem "C:\somefolder\somesubfolder" | ForEach-Object {
+
+        $Content = Get-Content $_.fullname 
+        
+        $Content = ForEach-Object { $Content -replace "@","O" }
+         
+        Set-Content $_.fullname $Content -Force
+
+}
