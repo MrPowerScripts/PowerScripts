80c26447961c4c02ffe2e0141227d6f1263711e6
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-04-22 19:11:00 -0400

    added RedditNinja.psm1 updated
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

--- Modules/RedditNinja/RedditNinja.psm1
+++ Modules/RedditNinja/RedditNinja.psm1
@@ -33,23 +33,28 @@ the lastest 25 from reddit.com, and display them
 
 #Get-Reddits -new
 
--forces Get-Reddits to download the lastest set of 25 from reddit.com
+    -forces Get-Reddits to download the lastest set of 25 from reddit.com
 
 #Get-Reddits -Index (integer set)
 
-- lets you show specific reddits from the stored index in three different ways
-    a. Get-Reddits -Index 5 --- for a single record inside the index
-    b. Get-Reddits -Index (1,5,13,25) --- will return only the index numbers specified
-    c. Get_Reddits -Index (5..7) --- will return the items between 5 and 7. i.e (5,6,7)
+    -lets you show specific reddits from the stored index in three different ways
+        a. Get-Reddits -Index 5 --- for a single record inside the index
+        b. Get-Reddits -Index (1,5,13,25) --- will return only the index numbers specified
+        c. Get_Reddits -Index (5..7) --- will return the items between 5 and 7. i.e (5,6,7)
 
 #Get-reddits -Browse (integer set)
 
-- Works the same way as -Index, except the values you provide here will launch those
-specific indexes into your default browser.
+    -Works the same way as -Index, except the values you provide here will launch those
+    specific indexes into your default browser.
+
+#Get-Reddits -sub SubRedditName
+    -downloads and displays subreddits from the specified subreddit
+
+    -Note: the -new and -browse can be used with -Sub as well
+        Get-Reddits -sub gaming -new
+        Get-Reddits -sub Gaming -browse 1,5,6
 
 #>
-
-
 
 function Get-Reddits {
     <#
@@ -62,85 +67,128 @@ function Get-Reddits {
     
     [Parameter(Mandatory=$False,HelpMessage="Enter one or more values 1 or (1,4,15) or (3..7)")]
     [ValidateRange(1,25)]
-    [array]
-    $Index=(1..25),
+    [array]$Index=(1..25),
 
     [Parameter(Mandatory=$False,HelpMessage="Enter a value between 1-25 to open this reddit page in a browser")]
     [ValidateRange(1,25)]
-    [array]
-    $Browse,
+    [array]$Browse,
     
     [Parameter(Mandatory=$False,HelpMessage="Use this flag alone to download new reddits")]
-    [Switch]
-    $New,
+    [switch]$New,
 
-    [Parameter(Mandatory=$False,HelpMesssage="Enter the name of a known subreddit to download the reddits")]
-    [ValidateLength(30)]
-    [string]
-    $Sub
+    [Parameter(Mandatory=$False,HelpMessage="Enter the name of a known subreddit to download the reddits")]
+    [AllowEmptyString()]
+    [string]$Sub
     )
+    cls
 
+    if ($Sub.length -eq 0) {
 
+        Write-Host "INSIDE MAIN" -BackgroundColor Green
+        if ($New -eq $True -or $MainStorage -eq $Null) {
+            
+            Write-Host "INSIDE MAIN NEW" -BackgroundColor Green
+            Write-Host "Downloading new main threads"
+            
+            try {
 
-    if ($Sub.length-gt0-eq$true) {
+                $WC = New-Object net.webclient         
+                $global:MainStorage = $WC.Downloadstring("http://www.reddit.com/.json") | ConvertFrom-json
+            
+            } catch {
 
-        Write-Host "Downloading new sub threads"
-        $WC = New-Object net.webclient
-        $global:SubRedditStorage = $WC.Downloadstring("http://www.reddit.com/r/$Sub/.json") | ConvertFrom-json
-        Write-Host "Threads Updated"
-
-    }
-
-    #Redownload the reddit jason which may have new items
-    if ($New-eq$true-or$RedditStorage-eq$Null) {
-        
-        if ($Sub.length-gt0-eq$true) {
-
-            Write-Host "Downloading new threads"
-            $WC = New-Object net.webclient
-            $global:RedditStorage = $WC.Downloadstring("http://www.reddit.com/.json") | ConvertFrom-json
+                Write-Host "Error: Did you type the correct Subreddit? Is Reddit.com down?" 
+            break function
+            }
+            
             Write-Host "Threads Updated"
         
-            break function
+        break function
+        }
+
+        if ($browse.count -gt 0 -eq $true) {
+            
+            $Browse | ForEach-Object {
+                $ix = $_-1
+
+                $RedditURI= "http://www.reddit.com"+$RedditStorage.data.children.Item(($ix)).data.permalink
+                Invoke-Expression "start $RedditURI"
+
+            }
+        break function
         }
         
-        
-        Write-Host "Downloading new threads"
-        $WC = New-Object net.webclient
-        $global:RedditStorage = $WC.Downloadstring("http://www.reddit.com/.json") | ConvertFrom-json
-        Write-Host "Threads Updated"
-        
-    break function
-    }
+        [array]::Reverse($Index)
 
-    #Browse switch to lauch a reddit thread in your default browser
-    if ($browse.count-gt0-eq$true) {
-
-        $Browse | ForEach-Object {
-            $ix = $_-1
-            $RedditURI= "http://www.reddit.com"+$RedditStorage.data.children.Item(($ix)).data.permalink
-            Invoke-Expression "start $RedditURI"
-        }
-    break function
-    }
-
-    #Reverse the array to show the most popular items at the bottom
-    [array]::Reverse($Index)
-
-    #Pass the items in the array to the foreach object, and display the content based on the index values submitted
-    $Index | ForEach-Object {
+        $Index | ForEach-Object {
         
         $ix = $_-1
 
         "`n_ $_ ______________________________________________________"
-        "title-------- "+$RedditStorage.data.children.Item($ix).data.title
-        "url---------- "+$RedditStorage.data.children.Item($ix).data.url
-        "PermaLink---- www.reddit.com"+$RedditStorage.data.children.Item($ix).data.permalink
-        "score-------- "+$RedditStorage.data.children.Item($ix).data.score
-        "ups---------- "+$RedditStorage.data.children.Item($ix).data.ups
-        "downs-------- "+$RedditStorage.data.children.Item($ix).data.downs
-        "author------- "+$RedditStorage.data.children.Item($ix).data.author
-        "Num_Comments- "+$RedditStorage.data.children.Item($ix).data.num_comments
-    }
-}
+        "title-------- "+$MainStorage.data.children.Item($ix).data.title
+        "url---------- "+$MainStorage.data.children.Item($ix).data.url
+        "PermaLink---- www.reddit.com"+$MainStorage.data.children.Item($ix).data.permalink
+        "score-------- "+$MainStorage.data.children.Item($ix).data.score
+        "ups---------- "+$MainStorage.data.children.Item($ix).data.ups
+        "downs-------- "+$MainStorage.data.children.Item($ix).data.downs
+        "author------- "+$MainStorage.data.children.Item($ix).data.author
+        "Num_Comments- "+$MainStorage.data.children.Item($ix).data.num_comments
+        }
 
+    } elseif ($Sub.length -gt 0) {
+
+        if ($new -eq $True -or $Sub -ne $SubReddit) {
+
+            Write-Host "Downloading new sub threads"
+            
+            try {
+            
+                $WC = New-Object net.webclient
+                $global:SubStorage = $WC.Downloadstring("http://www.reddit.com/r/$sub/.json") | ConvertFrom-json
+            
+            } catch {
+
+                Write-Host "Error: Did you type the correct Subreddit? Is Reddit.com down?" 
+            
+            break function
+            }
+            
+            Write-Host "Threads Updated"
+
+        }
+
+        if ($browse.count -gt 0 -eq $true) {
+
+            $Browse | ForEach-Object {
+                $ix = $_-1
+                $SubRedditURI= "http://www.reddit.com"+$SubStorage.data.children.Item(($ix)).data.permalink
+                Invoke-Expression "start $SubRedditURI"
+            }
+        
+        break function
+        
+        }
+
+        $Global:Subreddit=$Sub
+
+            [array]::Reverse($Index)
+
+            $Index | ForEach-Object {
+            
+            $ix = $_-1
+
+            "`n_ $_ ______________________________________________________"
+            "title-------- "+$SubStorage.data.children.Item($ix).data.title
+            "url---------- "+$SubStorage.data.children.Item($ix).data.url
+            "PermaLink---- www.reddit.com"+$SubStorage.data.children.Item($ix).data.permalink
+            "score-------- "+$SubStorage.data.children.Item($ix).data.score
+            "ups---------- "+$SubStorage.data.children.Item($ix).data.ups
+            "downs-------- "+$SubStorage.data.children.Item($ix).data.downs
+            "author------- "+$SubStorage.data.children.Item($ix).data.author
+            "Num_Comments- "+$SubStorage.data.children.Item($ix).data.num_comments
+            
+            }
+        
+        }
+    }#End Else
+#End Get-Reddits
