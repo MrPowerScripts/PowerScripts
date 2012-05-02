b5797b9beebbaff7cae93691cd1af84174a3cf01
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-05-02 17:38:56 -0400

    added CompareAgainstType.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Variables/CompareAgainstType.ps1
@@ -0,0 +1,20 @@
+﻿cls
+$Value = "4543",12
+
+
+if ($Value -is [int]) {
+Write-Host "$Value is a int"
+}
+
+if ($Value -is [string]) {
+Write-Host "$Value is a string"
+}
+
+
+if ($Value -is [array]) {
+Write-Host "$Value is a array"
+
+    if ($Value[1] -is [int]) {
+        Write-Host "The Second Value of the array is an int"
+    }
+}
