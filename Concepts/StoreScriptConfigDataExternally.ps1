dedbd9c92600e844f68d3aa0c730a6b44c61565c
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-05-10 20:43:54 -0400

    added StoreScriptConfigDataExternally.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Concepts/StoreScriptConfigDataExternally.ps1
@@ -0,0 +1,34 @@
+﻿   
+ <#
+
+
+Placing values in test.txt like this:
+
+One=1
+Two=2
+Three=3
+
+will allow you to call the values from powershell like this:
+
+$ConfigKeys.One
+$Configkeys.Two
+$ConfigKeys.Three
+
+to display the stored values
+
+#>
+  
+#File with the stored data
+$ConfigFile = "C:\SomeFile\test.txt"
+#Creating an empty hash table
+$ConfigKeys = @{}
+
+
+#Pulling, separating, and storing the values in $ConfigKey
+Get-Content $ConfigFile | ForEach-Object {
+
+$Keys = $_ -split "="
+
+$ConfigKey += @{$Keys[0]=$Keys[1]}
+
+}    
