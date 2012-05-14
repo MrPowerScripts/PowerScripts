ab1f9811ae53151860fce315f88bc77fd565b1d4
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-05-14 10:46:36 -0400

    added HashFromFilesInFolderRecursive.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Tools/HashFromFilesInFolderRecursive.ps1
@@ -0,0 +1,26 @@
+﻿#Folders for input and output
+$ParentFolder = "C:\users\Fern\Desktop\scripts"
+$ExportFile = "C:\users\Fern\Desktop\supertest.txt"
+
+#Can use md5, sha1, and more depending on your .net version
+$Type="md5"
+
+#Looping through the files, generating hash, and appending to export file
+$Files = Get-ChildItem $Parentfolder -Recurse -File
+
+#Create Empty Array To Store Hashes
+$Hashes=@()
+
+$Files | ForEach-Object {
+
+    $fs = new-object System.IO.FileStream $_.fullname, "Open"
+    $algo = [type]"System.Security.Cryptography.$Type"
+	$crypto = $algo::Create()
+    $hash = [BitConverter]::ToString($crypto.ComputeHash($fs)).Replace("-", "")
+    $fs.Close()
+    
+    $Hashes += ($_.fullname.substring(3) + " $hash")
+    
+}
+
+$Hashes | Out-File $ExportFile -Force
