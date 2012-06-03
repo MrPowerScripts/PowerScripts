e6be2f05b982fe9d1b388e1aa4d10324ed2a44b6
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-06-03 16:54:24 -0400

    Added Read-Host.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Cmdlets/Read-Host.ps1
@@ -0,0 +1,10 @@
+﻿cls
+#Assigns the input to a variable, or remove "$Var=" to pass the input to the pipeline
+$Var = Read-Host -Prompt "Please Enter Your Name" -AsSecureString #| Write-Host -ForegroundColor Green
+
+
+
+#This code will allow you to convert the secured string back to plain text
+#Add "$Var2 =" to the second line of code to add it into a variable
+$marshal = [Runtime.InteropServices.Marshal]
+$marshal::PtrToStringAuto($marshal::SecureStringToBSTR($Var))
--- Reports/GetProcessWebReport.ps1
+++ Reports/GetProcessWebReport.ps1
@@ -1,1 +1,4 @@
-﻿Get-Process | Where {$_.ProcessName -imatch "$Name"} | Select-Object Name, CPU | Sort-Object CPU -Descending  | ConvertTo-Html -Head "<style> th {background-color: red;} td {border: 1px black solid; padding: 3px;} </style>" -Title "Processnames" | Out-File -FilePath $Path
+﻿$Name="Chrome"
+$Path=$env:HOMEPATH+"\desktop\HTMLTest.html"
+
+Get-Process | Where {$_.ProcessName -imatch "$Name"} | Select-Object Name, CPU | Sort-Object CPU -Descending  | ConvertTo-Html -Head "<style> th {background-color: red;} td {border: 1px black solid; padding: 3px;} </style>" -Title "Processnames" | Out-File -FilePath $Path
