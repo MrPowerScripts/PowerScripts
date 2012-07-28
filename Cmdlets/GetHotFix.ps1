f8a5028cd6b8a1511333f2a5ad909e77b982f338
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-07-28 16:50:49 -0400

    Added GetHotFix.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Cmdlets/GetHotFix.ps1
@@ -0,0 +1,18 @@
+﻿#$HotFixes = Get-HotFix
+
+<#
+foreach ($HotFix in $HotFixes) {
+
+    if ($HotFix.Installedby -ilike "*fern*") {
+
+        Write-Host $Hotfix.HotFixID
+    }
+}
+#>
+
+
+<#
+$List = $HotFixes.GetEnumerator() | Select-Object -ExpandProperty Hotfixid
+
+Out-File "$env:HOMEDRIVE\$env:HOMEPATH\desktop\$Env:COMPUTERNAME.txt" -InputObject $List
+#>
