f87a7090f882c07d034ff40b175c20c0aece1a73
Mr Power Scripts <PlayingWithPowershell@gmail.com>
2012-09-05 16:18:56 -0300

    Update Reports/GetProcessWebReport.ps1

--- Reports/GetProcessWebReport.ps1
+++ Reports/GetProcessWebReport.ps1
@@ -1,4 +1,4 @@
 ﻿$Name="Chrome"
-$Path=$env:HOMEPATH+"\desktop\HTMLTest.html"
+$Path=$env:USERPROFILE+"\desktop\HTMLTest.html"
 
 Get-Process | Where {$_.ProcessName -imatch "$Name"} | Select-Object Name, CPU | Sort-Object CPU -Descending  | ConvertTo-Html -Head "<style> th {background-color: red;} td {border: 1px black solid; padding: 3px;} </style>" -Title "Processnames" | Out-File -FilePath $Path
