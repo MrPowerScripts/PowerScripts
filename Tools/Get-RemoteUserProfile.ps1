41f088bb0ecf5cae654cc34daf2793a4bfaac9e9
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-10-04 20:47:06 -0400

    added Get-RemoteUserProfile.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Tools/Get-RemoteUserProfile.ps1
@@ -0,0 +1,18 @@
+﻿
+function Get-RemoteProfile {
+
+param (
+[parameter(mandatory=$true)]
+$Computer
+)
+
+$Cred = Get-Credential
+
+Get-WmiObject -Class win32_userprofile -ComputerName $Computer -Credential $Cred | ft localpath,
+    
+    @{Name="LastUse";Expression={
+    [System.Management.ManagementDateTimeconverter]::ToDateTime($_.lastusetime) 
+                                }
+    } -AutoSize
+
+}
