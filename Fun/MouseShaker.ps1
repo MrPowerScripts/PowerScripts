31376911bd6b8f720f8c1c3dd074aca59d239972
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-11-18 14:16:36 -0500

    added MouseShaker.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

---
+++ Fun/MouseShaker.ps1
@@ -0,0 +1,29 @@
+﻿$Cursor = [system.windows.forms.cursor]::Clip
+
+#[system.windows.forms.cursor]::Position = New-Object system.drawing.point(0,0)
+
+#[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Cursor.Width/2),($Cursor.Height/2))
+
+
+
+for ($i=1;$i -lt 5000;$i++) {
+
+$Position = [system.windows.forms.cursor]::Position
+
+$PositionChange = Get-Random 20
+
+switch (Get-Random 4) {
+
+    0 {[system.windows.forms.cursor]::Position = New-Object system.drawing.point($Position.x, ($Position.y + $PositionChange))}
+    1 {[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Position.x + $PositionChange),$Position.y)}
+    2 {[system.windows.forms.cursor]::Position = New-Object system.drawing.point($Position.x, ($Position.y - $PositionChange))}
+    3 {[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Position.x - $PositionChange),$Position.y)}
+
+}
+
+#$PositionChange
+$i
+
+}
+
+#>
