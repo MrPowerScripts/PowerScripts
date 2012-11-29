1c554e4ae14e3ac0dbbeccc66471889b8e4e9d8b
MrPowerScripts <PlayingWithPowershell@gmail.com>
2012-11-28 22:03:20 -0500

    Updated MouseShaker.ps1
    Signed-off-by: MrPowerScripts <PlayingWithPowershell@gmail.com>

--- Fun/MouseShaker.ps1
+++ Fun/MouseShaker.ps1
@@ -4,7 +4,9 @@
 
 #[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Cursor.Width/2),($Cursor.Height/2))
 
-
+#Must add this line of code for [system.windows.forms.cursor]::Position to work outside of ISE
+#Thanks to Meik1988 on youtube for schooling me
+[System.Reflection.Assembly]::LoadWithPartialName("system.windows.forms")
 
 for ($i=1;$i -lt 5000;$i++) {
 
