aff5d47872935587352c476f686913eecfdd5eec
Mr Power Scripts <PlayingWithPowershell@gmail.com>
2013-06-22 06:58:51 -0700

    Added ValidateIPAddress.ps1

---
+++ Tools/ValidateIPAddress.ps1
@@ -0,0 +1,10 @@
+﻿
+for (;;) {
+
+$IP = Read-Host -Prompt "Please enter a valid IP address"
+
+if ([bool]($IP -as [ipaddress])) {Write-Host "Thank you" -f Green;break} else {Write-Host "Invalid IP. You entered $IP" -f Red}
+
+}
+
+$IP
