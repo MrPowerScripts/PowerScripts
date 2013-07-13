d3ab22836b75acf2fda0657c4f650fcbca9f62f2
Mr Power Scripts <PlayingWithPowershell@gmail.com>
2013-07-13 14:19:44 -0700

    Added Outlook folder added SendEmailFromOutlook.ps1

---
+++ Outlook/SendEmailFromOutlook.ps1
@@ -0,0 +1,24 @@
+﻿$OL = New-Object -ComObject outlook.application
+
+Start-Sleep 5
+
+<#
+olAppointmentItem
+olContactItem
+olDistributionListItem
+olJournalItem
+olMailItem
+olNoteItem
+olPostItem
+olTaskItem
+#>
+
+#Create Item
+$mItem = $OL.CreateItem("olMailItem")
+
+$mItem.To = "PlayingWithPowershell@gmail.com"
+$mItem.Subject = "PowerMail"
+$mItem.Body = "SENT FROM POWERSHELL"
+
+$mItem.Send()
+
