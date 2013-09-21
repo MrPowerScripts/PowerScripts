051b3f08b69db827080b580db65d1b482077254d
Mr Power Scripts <PlayingWithPowershell@gmail.com>
2013-09-21 09:06:05 -0700

    Added SendMailMessage.ps1

---
+++ Cmdlets/SendMailMessage.ps1
@@ -0,0 +1,23 @@
+$MyEmail = "PlayingWithPowershell@gmail.com"
+$SMTP= "smtp.gmail.com"
+$To = "PlayingWithPowershell@gmail.com"
+$Subject = "BRO!"
+$Body = "WHAT UP MR.POWERSCRIPTS?"
+$Creds = (Get-Credential -Credential "$MyEmail")
+
+Start-Sleep 2
+
+Send-MailMessage -To $to -From $MyEmail -Subject $Subject -Body $Body -SmtpServer $SMTP -Credential $Creds -UseSsl -Port 587 -DeliveryNotificationOption never
+
+<# 
+$PSEmailServer variable can be used to pre-configure the
+SMTP server in your Powershell Profile
+
+
+Delivery Notification Options:
+-- None: No notification.        
+-- OnSuccess: Notify if the delivery is successful.      
+-- OnFailure: Notify if the delivery is unsuccessful.     
+-- Delay: Notify if the delivery is delayed.       
+-- Never: Never notify.
+#>
